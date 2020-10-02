f8f17661b203dfedc11c377964f89ea5cc5300b3
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-10-02 10:23:15 +0200

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='integral-data-mirror',
-    version='1.0',
+    version='1.1.0',
     py_modules=["ddosadm","datamirror"],
     scripts=["download_data.sh"],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
